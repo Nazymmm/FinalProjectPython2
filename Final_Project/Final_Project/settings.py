83b08a9e917c3b5e9cf7549eb30d7efd721ea151
Nazymmm <80394333+Nazymmm@users.noreply.github.com>
2022-03-04 16:52:02 +0600

    Update settings.py

--- Final_Project/Final_Project/settings.py
+++ Final_Project/Final_Project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$+o05+ty@w$+l970v)umqchuy#4hd8l5c%u0&j)t=j7d&!*-%k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://yernur-kydyrov.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://final-project-python2.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
